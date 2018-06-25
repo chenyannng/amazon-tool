7d603abe8e3d474815819385b4a52130b22679ab
Dmytro Shevchenko <dshevchenko.mail@gmail.com>
2018-06-25 17:20:46 +0300

    code update

--- amazon.py
+++ amazon.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def page_main():
+def page_home():
     return render_template('home.html')
+
+
+@app.route('/about')
+def page_about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
